7aed922e442993b35a25c951795abdc3e3785e3e
Oscar Jiménez Bou <oscarjimenezbou@wifialum9-57.wi-fi.upv.es>
2024-02-23 08:33:06 +0100

    nothing

---
+++ P1/src.py
@@ -0,0 +1,12 @@
+from spicy.io import wavfile
+
+
+# # Specify the path to your audio file
+# audio_file = 'path/to/your/audio/file.wav'
+
+# # Load the audio file
+# data, sample_rate = sf.read(audio_file)
+
+# # Print the audio data and sample rate
+# print('Audio data:', data)
+# print('Sample rate:', sample_rate)
