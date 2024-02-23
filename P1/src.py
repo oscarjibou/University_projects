e7646516598f79edd95aaab188aeb4a694e9b422
Oscar Jiménez Bou <oscarjimenezbou@wifialum9-57.wi-fi.upv.es>
2024-02-23 10:02:24 +0100

    create jupyter and plot audio file

--- P1/src.py
+++ P1/src.py
@@ -1,12 +1,27 @@
-# from spicy.io import wavfile
+import simpleaudio as sa
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.io import wavfile
 
 
-# # Specify the path to your audio file
-# audio_file = 'path/to/your/audio/file.wav'
+archivo_wav = 'Practicas_Practica1_sound1.wav'
 
-# # Load the audio file
-# data, sample_rate = sf.read(audio_file)
+# # Load a wave file and play it
+# wave_obj = sa.WaveObject.from_wave_file(archivo_wav)
+# play_obj = wave_obj.play()
+# play_obj.wait_done()
 
-# # Print the audio data and sample rate
-# print('Audio data:', data)
-# print('Sample rate:', sample_rate)
+#plot the wave file in discrete time
+frecuencia_muestreo, datos = wavfile.read(archivo_wav)
+
+# Crear el eje de tiempo. El número de datos dividido por la frecuencia de muestreo nos da el tiempo total de la grabación.
+tiempo = 1/frecuencia_muestreo * np.arange(0, len(datos))
+
+# Graficar el archivo WAV en tiempo discreto
+plt.figure(figsize=(12, 6))
+plt.plot(tiempo, datos)
+plt.title('Señal de audio en tiempo discreto')
+plt.xlabel('Tiempo [s]')
+plt.ylabel('Amplitud')
+plt.show()
+
