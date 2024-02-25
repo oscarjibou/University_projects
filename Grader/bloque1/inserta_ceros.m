76a021d8e054d8a90120d840090ce84a191520f3
Oscar Jiménez Bou <oscarjimenezbou@MacBook-Pro-de-Oscar.local>
2024-02-25 21:51:06 +0100

    doesnot work insertaceros

--- Grader/bloque1/inserta_ceros.m
+++ Grader/bloque1/inserta_ceros.m
@@ -6,13 +6,18 @@ function y = inserta_ceros(x, L)
     %Salidas:
     % y, vector interpolado
         
+    if L <= 1
+        error('L debe ser mayor que 1');
+    end
+
     x = x(:)'; % Así nos aseguramos de que la entrada siempre sea un vector columna
     
     %Inserte su codigo a partir de aqui
         
     y = zeros(1, length(x)*L);
-    y(1:L:end) = x;
-    
+    y(1:L:end) = x; % Insertamos los valores de x en las posiciones 1, L+1, 2L+1, 3L+1, etc.
+                   % y(1:L:end) = x reemplaza cada L-ésimo elemento de y con el elemento correspondiente de x.
+
     end
     
     %matlab -batch "x = 1:4;L=2;y = inserta_ceros(x,L)"
