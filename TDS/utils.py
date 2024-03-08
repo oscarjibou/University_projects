c20521a9d1da98eab4c687c2f69d137edf5497b3
oscar jimenez <oscarjibou@gmail.com>
2024-03-08 12:31:42 +0100

    2.1 done

---
+++ TDS/utils.py
@@ -0,0 +1,21 @@
+from matplotlib import pyplot as plt
+from numpy.typing import ArrayLike
+
+def continuous_time_plot(*args: ArrayLike, variable_name: str, xlabel="Time (s)"):
+
+    plt.figure(figsize=(10, 5))
+    plt.plot(*args)
+    plt.title("Señal en el dominio del tiempo continuo")
+    plt.xlabel("Tiempo [s]")
+    plt.ylabel("Amplitud")
+    plt.show()
+
+
+def discrete_time_plot(*args: ArrayLike, variable_name: str, xlabel="Time (s)"):
+
+    plt.figure(figsize=(10, 5))
+    plt.stem(*args)
+    plt.title("Señal en el dominio del tiempo discreto")
+    plt.xlabel("Tiempo [s]")
+    plt.ylabel("Amplitud")
+    plt.show()
